dae60236e101af3daeb9f596e3c7c2a6420b3ebf
PavelDavydov <typical000@gmail.com>
2013-06-07 14:41:56 +0300

    Try to replace logo images for retina display

--- js/main.js
+++ js/main.js
@@ -90,6 +90,22 @@
           });
 
       });
+
+      // Replace normal images with retina images (find by class .hires)
+        $(function () {
+            if (window.devicePixelRatio == 2) {
+                var images = $("img.hires");
+                // loop through the images and make them hi-res
+                for(var i = 0; i < images.length; i++) {
+                    // create new image name
+                    var imageType = images[i].src.substr(-4);
+                    var imageName = images[i].src.substr(0, images[i].src.length - 4);
+                    imageName += "@2x" + imageType;
+                    //rename image
+                    images[i].src = imageName;
+                }
+            }
+        });
     });
 
     // On window resize we remake this horrible things :)
